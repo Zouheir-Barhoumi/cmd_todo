9dd9deb1a8ffc65d83bad6cdca41340a7884b523
Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>
2024-01-09 14:55:33 +0100

    implemented load function

--- todo.go
+++ todo.go
@@ -1,7 +1,9 @@
 package todo
 
 import (
+	"encoding/json"
 	"errors"
+	"os"
 	"time"
 )
 
@@ -42,5 +44,23 @@ func (t *Todos) Delete(number int) error {
 		return errors.New("invalid index number")
 	}
 	*t = append(items[:number-1], items[number:]...)
+	return nil
+}
+
+func (t *Todos) Load(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+	if len(file) == 0 {
+		return err
+	}
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
 	return nil
 }
